7ee33857aee12a5ac0d4033bb788a2b49b62fdcd
Dbibih Othmane <119870891+DbibihOthmane@users.noreply.github.com>
2025-12-23 17:50:32 +0100

    Update HelloController.java

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
    @GetMapping
 public String hello() {
-    return "Hello from New test00010 :)";
+    return "Hello from New test0001000 :)";
 }
 
     @GetMapping("/user")
